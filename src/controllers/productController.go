5354093d3aa4897190a88154b66a0eb626a3ad52
ono5 <kuroneko_head_lord@yahoo.co.jp>
2021-06-11 07:23:08 +0900

    feat: Product APIを追加

---
+++ src/controllers/productController.go
@@ -0,0 +1,78 @@
+// productController.go
+package controllers
+
+import (
+	"admin/src/database"
+	"admin/src/models"
+	"strconv"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func Products(ctx *fiber.Ctx) error {
+	var products []models.Product
+
+	// 全てのプロダクトを取得
+	database.DB.Find(&products)
+
+	return ctx.JSON(products)
+}
+
+func CreateProducts(ctx *fiber.Ctx) error {
+	var product models.Product
+
+	// リクエストデータをパースする
+	if err := ctx.BodyParser(&product); err != nil {
+		return err
+	}
+
+	// プロダクト取得
+	database.DB.Create(&product)
+
+	return ctx.JSON(product)
+}
+
+func GetProduct(ctx *fiber.Ctx) error {
+	// リクエストからIDを取得
+	id, _ := strconv.Atoi(ctx.Params("id"))
+
+	var product models.Product
+	product.ID = uint(id)
+
+	// プロダクト検索
+	database.DB.Find(&product)
+
+	return ctx.JSON(product)
+}
+
+func UpdateProduct(ctx *fiber.Ctx) error {
+	// リクエストからIDを取得
+	id, _ := strconv.Atoi(ctx.Params("id"))
+
+	product := models.Product{
+		ID: uint(id),
+	}
+
+	if err := ctx.BodyParser(&product); err != nil {
+		return err
+	}
+
+	// プロダクト更新
+	database.DB.Model(&product).Updates(&product)
+
+	return ctx.JSON(product)
+}
+
+func DeleteProduct(ctx *fiber.Ctx) error {
+	// リクエストからIDを取得
+	id, _ := strconv.Atoi(ctx.Params("id"))
+
+	product := models.Product{
+		ID: uint(id),
+	}
+
+	// プロダクト削除
+	database.DB.Delete(&product)
+
+	return nil
+}
--- src/routes/routes.go
+++ src/routes/routes.go
@@ -24,4 +24,9 @@ func Setup(app *fiber.App) {
 	adminAuthenticated.Put("info", controllers.UpdateInfo)
 	adminAuthenticated.Put("password", controllers.UpdatePassword)
 	adminAuthenticated.Get("ambassadors", controllers.Ambassadors)
+	adminAuthenticated.Get("products", controllers.Products)
+	adminAuthenticated.Post("products", controllers.CreateProducts)
+	adminAuthenticated.Get("products/:id", controllers.GetProduct)
+	adminAuthenticated.Put("products/:id", controllers.UpdateProduct)
+	adminAuthenticated.Delete("products/:id", controllers.DeleteProduct)
 }
