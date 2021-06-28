a55f9261d1b4ff9c5935c1625f32833e27fa63e4
ono5 <kuroneko_head_lord@yahoo.co.jp>
2021-06-29 07:39:04 +0900

    feat: Added register feature on frontend

---
+++ react-admin/src/models/user.ts
@@ -0,0 +1,20 @@
+// models/user.ts
+
+export class User {
+	first_name: string
+	last_name: string
+	email: string
+	password: string
+	password_confirm: string
+
+	constructor(
+		first_name: string, last_name: string, email: string,
+		password: string, password_confirm: string) {
+
+		this.first_name = first_name
+		this.last_name = last_name
+		this.email = email
+		this.password = password
+		this.password_confirm = password_confirm
+	}
+}
