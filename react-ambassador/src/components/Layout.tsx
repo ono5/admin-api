416076bf77bb595954b659f0fb63a2220e06fad0
ono5 <kuroneko_head_lord@yahoo.co.jp>
2021-07-15 07:30:32 +0900

    component and page nad routeing

---
+++ react-ambassador/src/components/Layout.tsx
@@ -0,0 +1,21 @@
+// components/Layout.tsx
+import Nav from './Nav'
+import Header from './Header'
+
+const Layout = (props: any) => {
+	return (
+		<div>
+			<Nav />
+			<main>
+				<Header />
+				<div className="album py-5 bg-light">
+					<div className="container">
+						{props.children}
+					</div>
+				</div>
+			</main>
+		</div>
+	)
+}
+
+export default Layout
